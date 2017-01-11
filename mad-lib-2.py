220cd33bff6dcd209a81186b227bfedb1d8eb4b7
Ocho262 <Brewsterm@chelseapride.org>
2017-01-11 17:43:48 -0500

    I added mad-lib-2
    I made my Mad Lib program say a different phrase.

---
+++ mad-lib-2.py
@@ -0,0 +1,10 @@
+# exercise 4
+
+noun = raw_input("Enter a noun, please.")
+verb = raw_input("Enter a verb, please.")
+adverb = raw_input("Enter an adverb, please.")
+adjective = raw_input ("Enter an adjective, please.")
+
+print("Can you " + verb + " " + adverb + " like your " + adjective + " " + noun + "? ")
+
+ 
