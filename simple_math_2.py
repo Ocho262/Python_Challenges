c6dc116a6d13e2e9e685a4e9d6e87492790c927c
Mbrew262 <Brewsterm@chelseapride.org>
2017-01-24 23:02:11 -0500

    This is the finshed simple math Python exercise.

---
+++ simple_math_2.py
@@ -0,0 +1,23 @@
+first_number = raw_input("What is the first number?")
+second_number = raw_input("What is the second number?")
+
+int_1 = int(first_number)
+int_2 = int(second_number)
+
+addition = int_1 + int_2
+subtraction = int_1 - int_2
+multiplication = int_1 * int_2
+division = int_1 / int_2
+
+simplemath = []
+simplemath.append(addition)
+simplemath.append(subtraction)
+simplemath.append(multiplication)
+simplemath.append(division)
+print(simplemath[0])
+print(simplemath[1])
+print(simplemath[2])
+print(simplemath[3])
+
+
+
