a8eaadba579bebdca1cf486f10b915d2531b05c8
Mbrew262 <Brewsterm@chelseapride.org>
2017-02-02 19:03:13 -0500

    This is the legal driving age Python exercise.

---
+++ legal_driving_age.py
@@ -0,0 +1,10 @@
+age = raw_input("What is your age?")
+
+a = int(age)
+
+if a < 16: 
+  print("You aren't old enough to legally drive yet. ")
+  
+if a >= 16: 
+  print("You are old enough to legally drive. ")
+  
