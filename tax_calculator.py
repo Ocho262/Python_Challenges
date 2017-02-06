887a2cc3b631761f93351569f07a9bb9265414e0
Ocho262 <Brewsterm@chelseapride.org>
2017-02-06 13:50:18 -0500

    Uploaded tax_calculator.py
    This is an incomplete version of tax_calculator.py

---
+++ tax_calculator.py
@@ -0,0 +1,22 @@
+amount = raw_input("What is the order amount?")
+state = raw_input("What is the state?")
+
+amount = int(raw_input)
+
+md_tax = $0.60
+md_tax = str(md_tax)
+
+if state = MD:
+    md_amount = md_tax + amount
+    md_amount = str(md_amount)
+
+print("The subtotal is " + amount + ". ")
+
+print("the tax is " + md_tax + ". ")
+
+print("the total is " + md_amount +". ")
+
+else:
+    print("The subtotal is " + amount + ". ")
+    print("The tax is $0.00. ")
+    print("The total is " + amount + ". ")
