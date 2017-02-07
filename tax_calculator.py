d8bf2ede2cb674b9f5eacbf0c5ab2b46043b294d
Ocho262 <brewsterm@chelseapride.com>
2017-02-06 23:26:21 -0500

    These are just a few edits. It doesn't print the right decimal place.

--- tax_calculator.py
+++ tax_calculator.py
@@ -62,22 +62,30 @@ states = {
 amount = raw_input("What is the order amount?")
 state = raw_input("What is the state?")
 
-amount_f = float(amount)
-amount_f = str(amount_f)
+amount = float(amount)
 
+md_tax = 0.06
+md_tax = float(md_tax)
+md_amount = md_tax * amount
+md_amount = float(md_amount)
 
+amount = str(amount)
+
+amount = repr(amount)
+
+md_tax = str(md_tax)
+
+md_amount = str(md_amount) 
+
+md_amount = repr(md_amount)
 
 
 if state == 'MD':
-    md_tax = 0.60
-    md_tax = str(md_tax)
-    md_amount = md_tax + amount
-    md_amount = str(md_amount)
     print("The subtotal is $" + amount + ". ")
     print("the tax is $" + md_tax + ". ")
     print("the total is $" + md_amount +". ")
 
-else:
+if state != 'MD':
     print("The subtotal is $" + amount + ". ")
     print("The tax is $0.00. ")
     print("The total is $" + amount + ". ")
