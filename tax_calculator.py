2a9fa70001fa74df48fe9bf082305cce8e1b000c
Ocho262 <brewsterm@chelseapride.com>
2017-02-10 15:52:37 -0500

    I added in a troubleshooting version of tax_calculator.py. Still isn't working exactly right, however.

--- tax_calculator.py
+++ tax_calculator.py
@@ -1,91 +1,22 @@
-states = {
-        'AK': 'Alaska',
-        'AL': 'Alabama',
-        'AR': 'Arkansas',
-        'AS': 'American Samoa',
-        'AZ': 'Arizona',
-        'CA': 'California',
-        'CO': 'Colorado',
-        'CT': 'Connecticut',
-        'DC': 'District of Columbia',
-        'DE': 'Delaware',
-        'FL': 'Florida',
-        'GA': 'Georgia',
-        'GU': 'Guam',
-        'HI': 'Hawaii',
-        'IA': 'Iowa',
-        'ID': 'Idaho',
-        'IL': 'Illinois',
-        'IN': 'Indiana',
-        'KS': 'Kansas',
-        'KY': 'Kentucky',
-        'LA': 'Louisiana',
-        'MA': 'Massachusetts',
-        'MD': 'Maryland',
-        'ME': 'Maine',
-        'MI': 'Michigan',
-        'MN': 'Minnesota',
-        'MO': 'Missouri',
-        'MP': 'Northern Mariana Islands',
-        'MS': 'Mississippi',
-        'MT': 'Montana',
-        'NA': 'National',
-        'NC': 'North Carolina',
-        'ND': 'North Dakota',
-        'NE': 'Nebraska',
-        'NH': 'New Hampshire',
-        'NJ': 'New Jersey',
-        'NM': 'New Mexico',
-        'NV': 'Nevada',
-        'NY': 'New York',
-        'OH': 'Ohio',
-        'OK': 'Oklahoma',
-        'OR': 'Oregon',
-        'PA': 'Pennsylvania',
-        'PR': 'Puerto Rico',
-        'RI': 'Rhode Island',
-        'SC': 'South Carolina',
-        'SD': 'South Dakota',
-        'TN': 'Tennessee',
-        'TX': 'Texas',
-        'UT': 'Utah',
-        'VA': 'Virginia',
-        'VI': 'Virgin Islands',
-        'VT': 'Vermont',
-        'WA': 'Washington',
-        'WI': 'Wisconsin',
-        'WV': 'West Virginia',
-        'WY': 'Wyoming'
-}
+subtotal = input("What is the order amount? ")
+print(subtotal)
+subtotal = float(subtotal)
 
-
-amount = raw_input("What is the order amount?")
-state = raw_input("What is the state?")
-
-amount = float(amount)
-
-md_tax = 0.06
-md_tax = float(md_tax)
-md_amount = md_tax * amount
-md_amount = float(md_amount)
-
-amount = str(amount)
-
-amount = repr(amount)
-
-md_tax = str(md_tax)
-
-md_amount = str(md_amount) 
-
-md_amount = repr(md_amount)
-
-
-if state == 'MD':
-    print("The subtotal is $" + amount + ". ")
-    print("the tax is $" + md_tax + ". ")
-    print("the total is $" + md_amount +". ")
-
-if state != 'MD':
-    print("The subtotal is $" + amount + ". ")
-    print("The tax is $0.00. ")
-    print("The total is $" + amount + ". ")
+f_subtotal = '{:20,.2f}'.format(subtotal)
+f_subtotal = str(f_subtotal)
+print("subtotal: " + f_subtotal)
+state = raw_input("What is the state? ")
+if (state == "MD"):
+    print("subtotal: " + f_subtotal)
+    taxrate = float(0.06)
+    tax = taxrate * subtotal
+    tax = float(tax)
+    f_tax = ('{:20,.2f}'.format(tax))
+    f_tax = str(f_tax)
+    print("Sales Tax: " + f_tax)
+    total = tax + subtotal
+    f_Total = ('{:20,.2f}'.format(total))
+    f_Total = str(f_Total)
+    print("Total: " + f_Total)
+else:
+    print("Don't know where that is.")
