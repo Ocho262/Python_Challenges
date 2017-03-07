6d2dc8c6a7bbe31ad373a6e8c9ba0356b998184f
Ocho262 <brewsterm@chelseapride.com>
2017-03-06 23:38:45 -0500

    This is the edit where I tried to add a possible solution, but it won't work. This solution seems the simplest and easiest to me, but I still need help getting it to work.

--- paint_calculator.py
+++ paint_calculator.py
@@ -1,11 +1,18 @@
 import math
 
-length = int(input("What is the length?"))
-width = int(input("What is the width? "))
+length = input("What is the length?")
+width = input("What is the width? ")
 
-ea_of_ceiling = length * width
-paint_needed = area_of_ceiling / const.paint
+CONST_PAINT = 350
+
+try:
+   val = int(length)
+   val = int(width)
+except ValueError:
+   print("That's not an int!")
+
+area_of_ceiling = length * width
+paint_needed = area_of_ceiling / CONST_PAINT
 paint_rounded = math.ceil(paint_needed)
-
 
 print("You will need to purchase {} gallons of paint to cover {} square feet.".format(paint_rounded, area_of_ceiling))
